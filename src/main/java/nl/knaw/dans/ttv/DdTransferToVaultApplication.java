3e9086778ccdc6b9c3850919d4a0ac0d9ff768f3
mderuijter <menko.de.ruijter@dans.knaw.nl>
2021-11-22 13:56:52 +0100

    fixed HibernateException

--- src/main/java/nl/knaw/dans/ttv/DdTransferToVaultApplication.java
+++ src/main/java/nl/knaw/dans/ttv/DdTransferToVaultApplication.java
@@ -19,6 +19,7 @@ package nl.knaw.dans.ttv;
 import io.dropwizard.Application;
 import io.dropwizard.db.PooledDataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
+import io.dropwizard.hibernate.UnitOfWorkAwareProxyFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import nl.knaw.dans.ttv.core.Inbox;
@@ -26,8 +27,11 @@ import nl.knaw.dans.ttv.core.Task;
 import nl.knaw.dans.ttv.core.TransferItem;
 import nl.knaw.dans.ttv.db.TransferItemDAO;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 
 public class DdTransferToVaultApplication extends Application<DdTransferToVaultConfiguration> {
@@ -56,7 +60,13 @@ public class DdTransferToVaultApplication extends Application<DdTransferToVaultC
     @Override
     public void run(final DdTransferToVaultConfiguration configuration, final Environment environment) {
         final TransferItemDAO transferItemDAO = new TransferItemDAO(hibernateBundle.getSessionFactory());
-        final List<Inbox> inboxes = configuration.buildInboxes();
+        List<Inbox> inboxes = new java.util.ArrayList<>(Collections.emptyList());
+
+        for (Map<String, String> inbox : configuration.getInboxes()) {
+            Inbox newInbox = new UnitOfWorkAwareProxyFactory(hibernateBundle)
+                    .create(Inbox.class, new Class[] {String.class, Path.class}, new Object[] {inbox.get("name"), Paths.get(inbox.get("path"))});
+            inboxes.add(newInbox);
+        }
 
         //get a list of sorted(creationTime) TransferTasks containing TransferItems, which have been checked for consistency disk/db
         List<Task<TransferItem>> tasks = new java.util.ArrayList<>(Collections.emptyList());
