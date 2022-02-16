1eb0da735cf95fa15015a60c1e95940a89fd9456
Eric de Vries <hro.mail@gmail.com>
2022-02-16 16:33:57 +0100

    Cleanup imports

--- src/main/java/nl/knaw/dans/ttv/core/TarRetryTaskCreator.java
+++ src/main/java/nl/knaw/dans/ttv/core/TarRetryTaskCreator.java
@@ -21,7 +21,6 @@ import nl.knaw.dans.ttv.core.service.TransferItemService;
 import nl.knaw.dans.ttv.db.Tar;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
