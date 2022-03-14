66b0727e6dce3cdcc078f80e2b55dfd7ba7059ea
Eric de Vries <hro.mail@gmail.com>
2022-03-14 17:49:04 +0100

    Fix missing fields and API endpoint

--- src/main/java/nl/knaw/dans/ttv/core/service/VaultCatalogServiceImpl.java
+++ src/main/java/nl/knaw/dans/ttv/core/service/VaultCatalogServiceImpl.java
@@ -18,11 +18,12 @@ package nl.knaw.dans.ttv.core.service;
 
 import nl.knaw.dans.ttv.db.Tar;
 import nl.knaw.dans.ttv.openapi.ApiException;
+import nl.knaw.dans.ttv.openapi.api.OcflObject;
 import nl.knaw.dans.ttv.openapi.api.TarPart;
-import nl.knaw.dans.ttv.openapi.api.TransferItem;
 import nl.knaw.dans.ttv.openapi.client.TarApi;
 
 import java.nio.charset.StandardCharsets;
+import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.stream.Collectors;
 
@@ -44,7 +45,8 @@ public class VaultCatalogServiceImpl implements VaultCatalogService {
         var apiTar = new nl.knaw.dans.ttv.openapi.api.Tar();
 
         apiTar.setTarUuid(tar.getTarUuid());
-        apiTar.setArchivalDate(tar.getDatetimeConfirmedArchived().atOffset(ZoneOffset.UTC));
+//        apiTar.setArchivalDate(tar.getDatetimeConfirmedArchived().atOffset(ZoneOffset.UTC));
+        apiTar.setArchivalDate(OffsetDateTime.now()); //tar.getDatetimeConfirmedArchived().atOffset(ZoneOffset.UTC));
         apiTar.setVaultPath(tar.getVaultPath());
         apiTar.setTarParts(tar.getTarParts().stream().map(p -> {
             var part = new TarPart();
@@ -55,23 +57,25 @@ public class VaultCatalogServiceImpl implements VaultCatalogService {
             return part;
         }).collect(Collectors.toList()));
 
-        apiTar.setTransferItems(tar.getTransferItems().stream().map(t -> {
-            var transferItem = new TransferItem();
-            transferItem.setBagId(t.getBagId());
-            transferItem.setObjectVersion("1");
-            transferItem.setDatastation(t.getDatasetDvInstance());
-            transferItem.setDataversePid(t.getDatasetPid());
-            transferItem.setDataversePidVersion(String.format("V%s.%s", t.getVersionMajor(), t.getVersionMinor()));
-            transferItem.setNbn(t.getNbn());
-            transferItem.setOtherId(t.getOtherId());
-            transferItem.setOtherIdVersion(t.getOtherIdVersion());
-            transferItem.setSwordClient(t.getSwordClient());
-            transferItem.setSwordToken(t.getSwordToken());
-            transferItem.setOcflObjectPath(t.getAipTarEntryName());
-            transferItem.setFilepidToLocalPath(new String(t.getPidMapping(), StandardCharsets.UTF_8));
-            transferItem.setMetadata(new String(t.getOaiOre(), StandardCharsets.UTF_8));
+        apiTar.setOcflObjects(tar.getTransferItems().stream().map(t -> {
+            var ocflObject = new OcflObject();
+            ocflObject.setBagId(t.getBagId());
+            ocflObject.setObjectVersion("1");
+            ocflObject.setDatastation(t.getDatasetDvInstance());
+            ocflObject.setDataversePid(t.getDatasetPid());
+            ocflObject.setDataversePidVersion(String.format("V%s.%s", t.getVersionMajor(), t.getVersionMinor()));
+            ocflObject.setNbn(t.getNbn());
+            ocflObject.setOtherId(t.getOtherId());
+            ocflObject.setOtherIdVersion(t.getOtherIdVersion());
+            ocflObject.setSwordClient(t.getSwordClient());
+            ocflObject.setSwordToken(t.getSwordToken());
+            ocflObject.setOcflObjectPath(t.getAipTarEntryName());
+            ocflObject.setFilepidToLocalPath(new String(t.getPidMapping(), StandardCharsets.UTF_8));
+            ocflObject.setMetadata(new String(t.getOaiOre(), StandardCharsets.UTF_8));
+            ocflObject.setVersionMajor(t.getVersionMajor());
+            ocflObject.setVersionMinor(t.getVersionMinor());
 
-            return transferItem;
+            return ocflObject;
         }).collect(Collectors.toList()));
 
         return apiTar;
